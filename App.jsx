cbbbb1b736bb2758580f566271ddace2010f75f9
titrom <71824652+titrom@users.noreply.github.com>
2023-01-17 16:51:38 +0200

    1

--- App.jsx
+++
@@ -1,6 +0,0 @@
-import Task1And2 from "./Tasks/task1And2";
-
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(
-    <Task1And2 N={2}/>
-);
--- Appi.js
+++
@@ -1,5 +0,0 @@
-import Task1And2 from "./Tasks/task1And2";
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render( /*#__PURE__*/React.createElement(Task1And2, {
-  N: 2
-}));
